7769462975d5c8f7133a2c94931942d76924a15b
Chris Kulwik <ckulwik@umich.edu>
2022-01-08 15:11:02 -0500

    icons are loading, need to add data next

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # weatherflow-map
+
+## In case of CORS issue:
+
+I had to download a chrome extension to get around a CORS issue:
+
+`Access to XMLHttpRequest at 'https://api.weatherflow.com/wxengine/rest/spot/getSpotDetailSetByZoomLevel?lat_min=33.82&lat_max=34.18&lon_min=-119.13&lon_max=-117.93&zoom=11&wf_token=d24d7c08a1e0136dca93e86cd38455a4&units_wind=mph&units_temp=f&units_distance=mi' from origin 'null' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.`
+
+### Steps
+
+- Add this extension to Chrome https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en
+- Once installed, go to the Chrome toolbar on the top of the page, click the 3 dots next to this extension, open up options.
+- change `4. Access-Control-Allow-Origin` to `*`
+- open the html page, and turn on the CORS extension.
+- CORS error should now be gone
--- scripts/init-map.js
+++ scripts/init-map.js
@@ -1,8 +1,35 @@
+const reqUrl =
+  "https://api.weatherflow.com/wxengine/rest/spot/getSpotDetailSetByZoomLevel?lat_min=33.82&lat_max=34.18&lon_min=-119.13&lon_max=-117.93&zoom=11&wf_token=d24d7c08a1e0136dca93e86cd38455a4&units_wind=mph&units_temp=f&units_distance=mi";
+
 let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 34.052, lng: -118.243 },
-    zoom: 8,
+    center: { lat: 34.052, lng: -118.343 },
+    zoom: 9,
   });
+
+  // var marker = new google.maps.Marker({
+  //   position: { lat: 34.052, lng: -118.243 },
+  //   title: "Location Place or Anything that you want to tooltip while hovering",
+  // });
+
+  // marker.setMap(map);
 }
+
+axios
+  .get(reqUrl)
+  .then((response) => {
+    response.data.spots.forEach((spot) => {
+      var marker = new google.maps.Marker({
+        position: { lat: spot.lat, lng: spot.lon },
+        title: spot.name,
+      });
+      console.log(spot);
+
+      marker.setMap(map);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
